b39c3b139f601a5acf862d370257d2edbf62be5e
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-01-08 16:10:37 +0100

    renamed to rdc

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var rss = require('./routes/rss');
+var rdc = require('./routes/rdc');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/rss', rss);
+app.use('/rss/rdc', rdc);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
